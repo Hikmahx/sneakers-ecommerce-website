f7573432f1609bdf7e7e1421eb0cb54ae51b3d38
Hikmahx <hikmayousuph@gmail.com>
2022-05-23 20:01:55 -1100

    middleware for verifying admin

--- middleware/auth.js
+++ middleware/auth.js
@@ -35,4 +35,13 @@ const verifyTokenAndAuthorization = (req, res, next) => {
   });
 };
 
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).send("You're not allowed to do that!");
+    }
+  });
+};
 module.exports = { verifyToken, verifyTokenAndAuthorization };
