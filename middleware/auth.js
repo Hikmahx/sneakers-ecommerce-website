e2c14da44c8713a1aacf1cf98dba560e801ca131
Hikmahx <hikmayousuph@gmail.com>
2022-05-23 19:49:41 -1100

    new authorization fxn added

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,31 +1,38 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 dotenv.config({ path: "../config/config.env" });
 
-module.exports = function(req, res, next){
+const verifyToken = (req, res, next) => {
   // Get token from header
-  const token = req.header('x-auth-token')
+  const token = req.header("x-auth-token");
 
   // Check if not token (check if the token doesn't exist)
-  if(!token){
+  if (!token) {
     // 401 unauthorized access
-    return res.status(401).json({msg: "No Token, authorization denied!"})
-  }"";
+    return res.status(401).json({ msg: "No Token, authorization denied!" });
+  }
 
   try {
-
     const decoded = jwt.verify(token, process.env.JWTSECRET);
-
 
     // Once verified, the payload will be put into decoded
     // We take the user out by taking the user id from the payload
     req.user = decoded.user;
-    next()
-
-
+    next();
   } catch (error) {
-    console.error(error.message)
-    res.status(401).send('Token is not valid')
+    console.error(error.message);
+    res.status(403).send("Token is not valid");
   }
+};
 
-}
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).send("You're not allowed to do that!");
+    }
+  });
+};
+
+module.exports = { verifyToken, verifyTokenAndAuthorization };
