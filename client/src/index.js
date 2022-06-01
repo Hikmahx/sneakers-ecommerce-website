f959014ba2160d3222cd03743ded2576bac76f62
Hikmahx <hikmayousuph@gmail.com>
2022-05-31 23:24:14 -1100

    store created

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
