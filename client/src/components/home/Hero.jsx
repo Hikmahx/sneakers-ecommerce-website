fc65af097f93b9aac76038aed9177be987bc1298
Hikmahx <hikmayousuph@gmail.com>
2022-06-10 13:27:05 -1100

    Hero updated

--- client/src/components/home/Hero.jsx
+++ client/src/components/home/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <section className="px-5 py-4 lg:py-24 mt-10 flex flex-col lg:flex-row items-center justify-between lg:mx-40 lg:px-1 max-w-xl md:max-w-xl mx-auto lg:max-w-7xl">
       <div
         id="hero-details"
-        className="container order-2 lg:order-1 text-center lg:text-left mx-auto lg:px-6 pt-5 sm:pt-10 lg:pt-5 pb-20 lg:pb-5 lg:pl-0 lg:pl-7 xl:mr-1 w-full lg:w-1/2"
+        className="container order-2 lg:order-1 text-center lg:text-left mx-auto lg:px-6 pt-5 sm:pt-10 lg:pt-5 pb-20 lg:pb-5 lg:px-0 xl:mr-1 w-full lg:w-1/2"
       >
         <h1 className="capitalize text-very-dark-blue font-bold text-3xl sm:text-4xl lg:text-6xl sm:leading-none pb-1 sm:pb-5">
           Discover your dream sneakers
