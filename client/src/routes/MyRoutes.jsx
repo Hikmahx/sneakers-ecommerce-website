411cb29e431dc5f73bb56109070ee0111496c2f3
Hikmahx <hikmayousuph@gmail.com>
2022-06-11 21:49:24 -1100

    myRoutes updated

--- client/src/routes/MyRoutes.jsx
+++ client/src/routes/MyRoutes.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home/Home";
+import About from "../pages/About";
+import Collections from "../pages/Collections";
+import Contact from "../pages/Contact";
+import Men from "../pages/Men";
+import Women from "../pages/Women";
 
 const MyRoutes = () => {
   return (
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/collections" element={<Collections />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/men" element={<Men />} />
+      <Route path="/women" element={<Women />} />
+    </Routes>
   );
 };
 
