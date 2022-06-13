3f87d0294b66f634760f303e4517262db6fe3830
Hikmahx <hikmayousuph@gmail.com>
2022-06-12 19:51:01 -1100

    all routes added

--- client/src/routes/MyRoutes.jsx
+++ client/src/routes/MyRoutes.jsx
@@ -6,16 +6,63 @@ import Collections from "../pages/Collections";
 import Contact from "../pages/Contact";
 import Men from "../pages/Men";
 import Women from "../pages/Women";
+import Login from "../pages/User/Login";
+import Register from "../pages/User/Register";
+import Layout from "../components/layout/Layout";
 
 const MyRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/collections" element={<Collections />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/men" element={<Men />} />
-      <Route path="/women" element={<Women />} />
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <Home />
+          </Layout>
+        }
+      />
+      <Route
+        path="/about"
+        element={
+          <Layout>
+            <About />
+          </Layout>
+        }
+      />
+      <Route
+        path="/collections"
+        element={
+          <Layout>
+            <Collections />
+          </Layout>
+        }
+      />
+      <Route
+        path="/contact"
+        element={
+          <Layout>
+            <Contact />
+          </Layout>
+        }
+      />
+      <Route
+        path="/men"
+        element={
+          <Layout>
+            <Men />
+          </Layout>
+        }
+      />
+      <Route
+        path="/women"
+        element={
+          <Layout>
+            <Women />
+          </Layout>
+        }
+      />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 };
