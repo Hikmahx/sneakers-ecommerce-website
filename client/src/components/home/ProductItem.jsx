fd2b2b78a7cbe23b1fda67e8327d540aaa28b758
Hikmahx <hikmayousuph@gmail.com>
2022-06-18 19:02:12 +0100

    Product Components updated

--- client/src/components/home/ProductItem.jsx
+++ client/src/components/home/ProductItem.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const ProductItem = () => {
   return (
-    <div className="product-card bg-white min-w-[4rem] w-full max-w-xs h-auto rounded-md shadow-md pb-12 mx-3 mb-6 cursor-pointer">
-      <div className="img-wrapper p-5 border-b border-grayish-blue hover:p-0 transition-all">
+    <div className="group relative">
+      <div className="w-full min-h-80 bg-grayish-blue aspect-w-1 aspect-h-1 rounded-md overflow-hidden  lg:h-80 lg:aspect-none relative before:absolute before:bg-[hsla(222,_4%,_48%,_0.4)] before:inset-0 text-center before:h-0 group-hover:before:h-full before:transition-all">
         <img
           src="https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=725&q=80"
           alt="product display"
-          className=""
+          className="w-full h-full object-center object-cover lg:w-full lg:h-full"
         />
       </div>
-      <div className="product-info p-5">
-        <h3 className="company uppercase text-orange font-bold text-xs tracking-wider pb-3">
-          sneaker company
-        </h3>
-        <h4 className="product capitalize text-very-dark-blue text-xl lg:text-2xl font-bold sm:leading-none pb-5">
-          fall limited edition sneakers
-        </h4>
-        <div className="price flex justify-between items-center">
-          <div className="discount-price text-xl font-bold">$125.00</div>
+      <div className="">
+        <a href="/">
+          <span aria-hidden="true" className="absolute inset-0" />
+          <p className="company uppercase text-orange font-bold text-[0.625rem] tracking-wider pb-2 pt-4">
+            sneaker company
+          </p>
+          <h3 className="product capitalize text-very-dark-blue text-lg font-bold sm:leading-none pb-4">
+            fall limited edition sneakers
+          </h3>
+        </a>
+        <div className="flex justify-between items-center text-sm">
+          <div className="discount-price font-bold">$125.00</div>
           <div className="original-price text-grayish-blue line-through">
             $250.00
           </div>
@@ -28,7 +31,33 @@ const ProductItem = () => {
         </div>
       </div>
     </div>
+    // <div className="product-card bg-white min-w-[4rem] w-full max-w-xs h-auto rounded-md shadow-md pb-12 mx-3 mb-6 cursor-pointer">
+    //   <div className="img-wrapper p-5 border-b border-grayish-blue hover:p-0 transition-all">
+    //     <img
+    //       src="https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=725&q=80"
+    //       alt="product display"
+    //       className=""
+    //     />
+    //   </div>
+    //   <div className="product-info p-5">
+    //     <h3 className="company uppercase text-orange font-bold text-xs tracking-wider pb-3">
+    //       sneaker company
+    //     </h3>
+    //     <h4 className="product capitalize text-very-dark-blue text-xl lg:text-2xl font-bold sm:leading-none pb-5">
+    //       fall limited edition sneakers
+    //     </h4>
+    //     <div className="price flex justify-between items-center">
+    //       <div className="discount-price text-xl font-bold">$125.00</div>
+    //       <div className="original-price text-grayish-blue line-through">
+    //         $250.00
+    //       </div>
+    //       <div className="discount text-orange bg-pale-orange font-bold w-max px-2 rounded mx-5 h-6">
+    //         50%
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
-}
+};
 
-export default ProductItem
+export default ProductItem;
--- client/src/components/home/Products.jsx
+++ client/src/components/home/Products.jsx
@@ -8,17 +8,23 @@ const Products = () => {
         <h2 className="text-very-dark-blue font-bold text-2xl text-center mx-auto lg:text-left sm:text-4xl sm:leading-none py-6 sm:pb-5 mb-6 lg:mb-9">
           Products
         </h2>
-        <div
+        {/* <div
           id="products"
           className="flex flex-wrap justify-center items-center mx-auto px-4 xl:-mx-2"
-        >
-          <ProductItem />
-          <ProductItem />
-          <ProductItem />
-          <ProductItem />
-          <ProductItem />
-          <ProductItem />
+        > */}
+        <div className="max-w-2xl mx-auto lg:max-w-7xl px-4 lg:px-0">
+          <div className="grid grid-cols-1 gap-y-12 sm:y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+          </div>
         </div>
+        {/* </div> */}
         <div className="mx-8">
           <button className="cart min-w-[4rem] w-full max-w-xs md:max-w-lg lg:max-w-none h-14 bg-pale-orange border border-orange rounded-lg mt-12 mx-auto text-orange font-bold flex items-center justify-center lg:w-52 shadow-[inset_0_0_0_0_#ff7d1a] hover:shadow-[inset_0_-4rem_0_0_#ff7d1a] hover:text-white transition-all duration-300">
             Explore More
