93a5d1aa05103b8a2f4c19936acd157bf9ad79cb
Hikmahx <hikmayousuph@gmail.com>
2022-06-18 20:41:03 +0100

    components completed

---
+++ client/src/pages/Women.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import WomenHeader from "../assets/page-header/women-header.jpg";
+import ProductItem from "../components/home/ProductItem";
+
+const Women = () => {
+  return (
+    <section className="h-auto pt-2 min-h-[80vh]">
+      <div className="max-w-xl sm:max-w-4xl lg:max-w-7xl relative px-5 pt-20 pb-12 items-center mx-auto lg:mx-20 xl:mx-28 2xl:mx-40 3xl:mx-auto lg:pb-2 lg:px-1 xl:px-3 2xl:px-1">
+        <h2 className="product capitalize text-white font-bold text-center relative z-10 lg:text-left text-3xl sm:text-4xl sm:leading-none pb-3 px-8">
+          Women
+        </h2>
+        <div className="absolute top-0 left-0 bg-dark-grayish-blue w-full h-48 rounded-md overflow-hidden">
+          <img
+            src={WomenHeader}
+            alt="two women sitting"
+            className="opacity-10 absolute h-full w-full object-cover"
+          />
+        </div>
+
+        <div className="product-container max-w-2xl mx-auto lg:max-w-7xl px-4 lg:px-0 my-32">
+          <div className="grid grid-cols-1 gap-y-12 sm:y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Women;
