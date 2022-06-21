bba5f69fe635470cd8408d191c0482e8e0da035b
Hikmahx <hikmayousuph@gmail.com>
2022-06-21 15:11:27 +0100

    Filter updated

--- client/src/components/Filter.jsx
+++ client/src/components/Filter.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Filter = () => {
+ const [filter, setFilter] = useState({})
+ const [sort, setSort] = useState({})
+
+
+ const handleFilter = (e) =>{
+    setFilter({...filter, [e.target.name]: e.target.value})
+ }
+
+//  console.log(filter)
+
   return (
-    <div className="wrapper mt-20 flex flex-col sm:flex-row justify-between max-w-xl sm:max-w-2xl md:max-7xl mx-auto">
-      <div className="filter-container mb-4">
+    <div className="wrapper mt-28 lg:mt-40 flex flex-col sm:flex-row justify-between mx-auto">
+      <div className="filter-container mb-4 sm:mb-0 flex items-center justify-between">
         <span className="font-bold sm:text-xl mr-2 sm:mr-10">Filter Products:</span>
+        <div className="flex">
         <select
-          className="form-select appearance-none px-3 py-1.5 border border-solid rounded transition ease-in-out m-0 focus:outline-none"
+          className="form-select appearance-none px-3 py-2 border border-solid transition ease-in-out m-0 focus:outline-none mr-4"
           aria-label="Default select example"
+          name="color"
+          onChange={handleFilter}
         >
           <option value="color">Color</option>
           <option value="white">White</option>
@@ -17,13 +30,29 @@ const Filter = () => {
           <option value="green">Green</option>
           <option value="yellow">Yellow</option>
         </select>
+        <select
+          className="form-select appearance-none px-3 py-2 border border-solid transition ease-in-out m-0 focus:outline-none capitalize"
+          aria-label="Default select example"
+          name="brand"
+          onChange={handleFilter}
+        >
+          <option value="brand">brand</option>
+          <option value="nike">nike</option>
+          <option value="adidas">adidas</option>
+          <option value="puma">puma</option>
+          <option value="reebok">reebok</option>
+          <option value="jordan">jordan</option>
+          <option value="vans">vans</option>
+        </select>
+        </div>
       </div>
-      <div className="sort-container">
+      <div className="sort-container flex items-center justify-between">
         <span className="font-bold sm:text-xl mr-2 sm:mr-10">Sort Products:</span>
         <select
-          className="form-select appearance-none px-3 py-1.5 border border-solid rounded transition ease-in-out m-0 focus:outline-none"
+          className="form-select appearance-none px-3 py-2 border border-solid transition ease-in-out m-0 focus:outline-none"
           aria-label="Default select example"
           name="sortBy"
+          onChange={(e)=>{setSort(e.target.value)}}
         >
           <option value="newest">Newest</option>
           <option value="asc">Price, low-high</option>
