0dcc5fae791d9b0bbca9f02b613a5e4720f0bf6e
Hikmahx <hikmayousuph@gmail.com>
2022-06-22 19:32:56 +0100

    Product model updated

--- models/Product.js
+++ models/Product.js
@@ -6,8 +6,9 @@ const ProductSchema = new mongoose.Schema(
     title: { type: String, required: true },
     desc: { type: String, required: true },
     img: { type: String, required: true },
-    category: { type: Array },
+    categories: { type: Array },
     size: { type: String },
+    color: {type: Array},
     price: { type: Number, required: true },
   },
   { timestamps: true }
