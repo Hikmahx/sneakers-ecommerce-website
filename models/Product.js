ddaf28724f4f09750bc41d84b80bbce5d4192a1e
Hikmahx <hikmayousuph@gmail.com>
2022-06-22 19:33:34 +0100

    Proudct model updated

--- models/Product.js
+++ models/Product.js
@@ -7,7 +7,7 @@ const ProductSchema = new mongoose.Schema(
     desc: { type: String, required: true },
     img: { type: String, required: true },
     categories: { type: Array },
-    size: { type: String },
+    size: { type: Array },
     color: {type: Array},
     price: { type: Number, required: true },
   },
