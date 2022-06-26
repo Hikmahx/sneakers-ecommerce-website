832c11de906b47078c91caa15776f997019b1c2e
Hikmahx <hikmayousuph@gmail.com>
2022-06-26 17:48:42 +0100

    DesktopPreview updated

--- client/src/components/ProductPage/DesktopPreview.jsx
+++ client/src/components/ProductPage/DesktopPreview.jsx
@@ -1,47 +1,42 @@
-import React, { createRef, useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { AllProducts } from "../../data";
-import { getProducts, activeThumbnail, activeModalThumbnail } from '../../redux/reducers/productSlice'
-
+import { changeImage } from "../../redux/reducers/productSlice";
 
 const DesktopPreview = ({ thumbnails }) => {
   const dispatch = useDispatch();
-
-useEffect(() => {
-  dispatch(activeThumbnail(thumbnailRef))
-  // eslint-disable-next-line    
-}, [])
-
-  const thumbnailRef = useRef()
-
+  const curIndex = useSelector((state) => state.product.curIndex);
+  const images = useSelector((state) => state.product.images);
 
   return (
     <>
       <div className="preview xl:min-w-md max-w-3xl rounded-2xl overflow-hidden cursor-pointer">
         <img
-          // onClick={lightBox}
-          src={AllProducts[0].img[0]}
+          src={images[curIndex]}
           alt="product-preview"
           className="transition duration-150 ease-in-out"
           data-bs-toggle="modal"
           data-bs-target="#exampleModal"
-          // data-bs-toggle="modal" data-bs-target="#exampleModal"
         />
       </div>
       <div
-        ref={thumbnailRef}
-        className={"thumbnails flex max-w-3xl pt-8 justify-between " + (AllProducts[10].img.length> 3? 'w-100' : "w-3/4 mx-auto")}
+        className={
+          "thumbnails flex max-w-3xl pt-8 justify-between " +
+          (images.length > 3 ? "w-100" : "w-3/4 mx-auto")
+        }
       >
-        {AllProducts[10].img.map((img, index) => (
+        {images.map((img, index) => (
           <div
-            // ref={modalThumbnailRef}
             key={index}
-            className="cursor-pointer w-22 h-22 rounded-xl hover:opacity-80 relative overflow-hidden bg-white"
+            className={
+              "cursor-pointer w-22 h-22 rounded-xl relative overflow-hidden bg-white transition-all" +
+              (curIndex === index ? " border-2 border-orange" : "")
+            }
           >
             <img
-              id={index}
-              // onClick={previewDisplay}
-              className="w-full"
+              onClick={() => dispatch(changeImage(index))}
+              className={
+                "w-full h-full hover:opacity-80 " +
+                (curIndex === index ? " opacity-50" : "")
+              }
               src={img}
               alt="thumbnail"
             />
