a61f15c88fe3752100704fe66b4d96a4f6cacb48
Hikmahx <hikmayousuph@gmail.com>
2022-06-27 00:10:31 +0100

    Filter updated

--- client/src/components/Filter.jsx
+++ client/src/components/Filter.jsx
@@ -1,58 +1,67 @@
 import React, { useState } from "react";
 
 const Filter = () => {
- const [filters, setFilters] = useState({})
- const [sort, setSort] = useState('newest')
+  const [filters, setFilters] = useState({});
+  const [sort, setSort] = useState("newest");
 
+  const handleFilter = (e) => {
+    setFilters({ ...filters, [e.target.name]: e.target.value });
+  };
 
- const handleFilter = (e) =>{
-    setFilters({...filters, [e.target.name]: e.target.value})
- }
-
-//  console.log(filter)
+  //  console.log(filter)
 
   return (
     <div className="wrapper mt-28 lg:mt-40 flex flex-col sm:flex-row justify-between mx-auto">
       <div className="filter-container mb-4 sm:mb-0 flex items-center justify-between">
-        <span className="font-bold sm:text-xl mr-2 sm:mr-10">Filter Products:</span>
+        <span className="font-bold sm:text-xl mr-2 sm:mr-10">
+          Filter Products:
+        </span>
         <div className="flex">
-        <select
-          className="form-select appearance-none px-3 py-2 border border-solid transition ease-in-out m-0 focus:outline-none mr-4"
-          aria-label="Default select example"
-          name="color"
-          onChange={handleFilter}
-        >
-          <option value="color" selected disabled>Color</option>
-          <option value="white">White</option>
-          <option value="black">Black</option>
-          <option value="orange">Orange</option>
-          <option value="red">Red</option>
-          <option value="green">Green</option>
-          <option value="yellow">Yellow</option>
-        </select>
-        <select
-          className="form-select appearance-none px-3 py-2 border border-solid transition ease-in-out m-0 focus:outline-none capitalize"
-          aria-label="Default select example"
-          name="brand"
-          onChange={handleFilter}
-        >
-          <option value="brand" selected disabled>brand</option>
-          <option value="nike">nike</option>
-          <option value="adidas">adidas</option>
-          <option value="puma">puma</option>
-          <option value="reebok">reebok</option>
-          <option value="jordan">jordan</option>
-          <option value="vans">vans</option>
-        </select>
+          <select
+            className=" appearance-none px-3 py-2 border border-solid transition ease-in-out m-0 focus:outline-none mr-4 bg-white"
+            aria-label="Default select example"
+            name="color"
+            onChange={handleFilter}
+          >
+            <option value="color" selected disabled>
+              Color
+            </option>
+            <option value="white">White</option>
+            <option value="black">Black</option>
+            <option value="orange">Orange</option>
+            <option value="red">Red</option>
+            <option value="green">Green</option>
+            <option value="yellow">Yellow</option>
+          </select>
+          <select
+            className=" appearance-none px-3 py-2 border border-solid transition ease-in-out m-0 focus:outline-none capitalize bg-white"
+            aria-label="Default select example"
+            name="brand"
+            onChange={handleFilter}
+          >
+            <option value="brand" selected disabled>
+              brand
+            </option>
+            <option value="nike">nike</option>
+            <option value="adidas">adidas</option>
+            <option value="puma">puma</option>
+            <option value="reebok">reebok</option>
+            <option value="jordan">jordan</option>
+            <option value="vans">vans</option>
+          </select>
         </div>
       </div>
       <div className="sort-container flex items-center justify-between">
-        <span className="font-bold sm:text-xl mr-2 sm:mr-10">Sort Products:</span>
+        <span className="font-bold sm:text-xl mr-2 sm:mr-10">
+          Sort Products:
+        </span>
         <select
-          className="form-select appearance-none px-3 py-2 border border-solid transition ease-in-out m-0 focus:outline-none"
+          className="appearance-none px-3 py-2 border border-solid transition ease-in-out m-0 focus:outline-none bg-white"
           aria-label="Default select example"
           name="sortBy"
-          onChange={(e)=>{setSort(e.target.value)}}
+          onChange={(e) => {
+            setSort(e.target.value);
+          }}
         >
           <option value="newest">Newest</option>
           <option value="asc">Price, low-high</option>
