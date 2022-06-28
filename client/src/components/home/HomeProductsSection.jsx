9db4a3cd050d4ea9fceed87280a832c58491a677
Hikmahx <hikmayousuph@gmail.com>
2022-06-28 07:52:03 +0100

    useState replace with redux

--- client/src/components/home/HomeProductsSection.jsx
+++ client/src/components/home/HomeProductsSection.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import ProductItem from "./ProductItem";
-
 const HomeProductsSection = () => {
-  const [products, setProducts] = useState([])
+  const products = useSelector((state) => state.product.products);
 
   return (
     <section className="bg-light-grayish-blue py-12">
@@ -17,17 +17,14 @@ const HomeProductsSection = () => {
         > */}
         <div className="max-w-2xl mx-auto lg:max-w-7xl px-4 lg:px-0">
           <div className="grid grid-cols-1 gap-y-12 sm:y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          { 
-              products.map((product, index)=>(
-                
-                <ProductItem key={product.id} product={product}/>
-              ))
-            }
+            {products.slice(0, 4).map((product, index) => (
+              <ProductItem key={product.id} product={product} />
+            ))}
           </div>
         </div>
         {/* </div> */}
         <div className="mx-8">
-          <Link to="products">            
+          <Link to="products">
             <button className="cart min-w-[4rem] w-full max-w-xs md:max-w-lg lg:max-w-none h-14 bg-pale-orange border border-orange rounded-lg mt-12 mx-auto text-orange font-bold flex items-center justify-center lg:w-52 shadow-[inset_0_0_0_0_#ff7d1a] hover:shadow-[inset_0_-4rem_0_0_#ff7d1a] hover:text-white transition-all duration-300">
               Explore More
             </button>
