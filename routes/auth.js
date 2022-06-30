f5baf3c6eaad8dba54c1d895f56c43f855c9df42
Hikmahx <hikmayousuph@gmail.com>
2022-06-30 22:49:45 +0100

    auth.js updated

--- routes/auth.js
+++ routes/auth.js
@@ -74,7 +74,7 @@ router.post(
           res.json({ token });
         }
       );
-    } catch (error) {
+    } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
     }
