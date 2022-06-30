810b6bc97772c9d67ee06601cb5c16594554cefd
Hikmahx <hikmayousuph@gmail.com>
2022-06-30 23:29:40 +0100

    product schema updated

--- models/Product.js
+++ models/Product.js
@@ -5,11 +5,17 @@ const ProductSchema = new mongoose.Schema(
     company: { type: String, required: true },
     title: { type: String, required: true },
     desc: { type: String, required: true },
-    img: { type: String, required: true },
-    categories: { type: Array },
+    img: { type: Array, required: true },
+    alt: { type: String, required: true },
+    categories: [
+      {
+        color: { type: Array, required: true },
+        gender: { type: Array, default: ["men", "women"] }
+      }
+    ],
     size: { type: Array },
-    color: {type: Array},
     price: { type: Number, required: true },
+    discountPrice: { type: Number, required: true },
   },
   { timestamps: true }
 );
