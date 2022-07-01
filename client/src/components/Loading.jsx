b85775e45d91b542118fe9771b2cc5b83f35d0c8
Hikmahx <hikmayousuph@gmail.com>
2022-07-01 12:46:00 +0100

    Loading component completed

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center space-x-2 mt-20">
+
+      <div
+        className="
+    spinner-border
+    animate-spin
+    inline-block
+    w-8
+    h-8
+    border-4
+    rounded-full
+    text-orange
+  "
+        role="status"
+      >
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
