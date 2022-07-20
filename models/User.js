788d423beeee28697ca04768b320088375711f3a
Hikmahx <hikmayousuph@gmail.com>
2022-07-20 01:34:46 +0100

    firstname and lastname added

--- models/User.js
+++ models/User.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
   {
+    firstname: { type: String, required: true, unique: true },
+    lastname: { type: String, required: true, unique: true },
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
