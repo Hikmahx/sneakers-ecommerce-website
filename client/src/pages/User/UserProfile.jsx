e24fc835a6be417e47aad67b81ad3533137fad4b
Hikmahx <hikmayousuph@gmail.com>
2022-07-22 22:52:48 +0100

    nav reorganised, outlet imported, style updated

--- client/src/pages/User/UserProfile.jsx
+++ client/src/pages/User/UserProfile.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserDetails } from "../../redux/reducers/authSlice";
 import Loading from "../../components/Loading";
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const UserProfile = () => {
   const { userInfo, loading, error, userErrorMsg } = useSelector(
@@ -19,7 +19,7 @@ const UserProfile = () => {
 
   return (
     <section className="h-auto pt-2 min-h-[80vh] bg-[#f9f9f9]">
-      <div className=" max-w-xl sm:max-w-4xl lg:max-w-7xl relative mt-16 px-5 pt-20 pb-12 items-center mx-auto lg:mx-20 xl:mx-28 2xl:mx-40 3xl:mx-auto lg:pb-2 lg:px-1 xl:px-3 2xl:px-1">
+      <div className=" max-w-xl lg:max-w-7xl relative px-5 py-20 items-center mx-auto lg:mx-20 xl:mx-28 2xl:mx-40 3xl:mx-auto lg:px-1 xl:px-3 2xl:px-1">
         {!error ? (
           <>
             {loading ? (
@@ -30,7 +30,7 @@ const UserProfile = () => {
                   <>
                     {" "}
                     <div className="flex gap-x-4 flex-col lg:flex-row">
-                      <div className="sm:bg-white lg:w-1/4 rounded-lg sm:shadow-md py-4">
+                      <div className="lg:bg-white lg:w-1/4 rounded-lg lg:shadow-md py-4">
                         <div className="profile-img-wrapper w-32 h-32 bg-grayish-blue rounded-full mx-auto relative">
                           <button className="w-5 h-5 absolute right-3">
                             <ion-icon
@@ -65,15 +65,39 @@ const UserProfile = () => {
                           </Link>
 
                           <Link
-                            to="settings"
+                            to="orders"
                             className=" text-dark-grayish-blue border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900 group border-l-4 px-3 py-2 flex items-center text-sm font-medium"
                             x-state-description='undefined: "bg-pale-orange border-orange text-dark-grayish-blue hover:bg-pale-orange hover:text-dark-grayish-blue", undefined: "border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900"'
                           >
                             <ion-icon
                               class="p-2 text-base"
-                              name="settings"
+                              name="basket"
                             ></ion-icon>
-                            <span className="truncate">Account setting</span>
+                            <span className="truncate">My Orders</span>
+                          </Link>
+
+                          <Link
+                            to="addresses"
+                            className=" text-dark-grayish-blue border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900 group border-l-4 px-3 py-2 flex items-center text-sm font-medium"
+                            x-state-description='undefined: "bg-pale-orange border-orange text-dark-grayish-blue hover:bg-pale-orange hover:text-dark-grayish-blue", undefined: "border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900"'
+                          >
+                            <ion-icon
+                              class="p-2 text-base"
+                              name="location"
+                            ></ion-icon>
+                            <span className="truncate">My Address</span>
+                          </Link>
+
+                          <Link
+                            to="notifications"
+                            className=" text-dark-grayish-blue border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900 group border-l-4 px-3 py-2 flex items-center text-sm font-medium"
+                            x-state-description='undefined: "bg-pale-orange border-orange text-dark-grayish-blue hover:bg-pale-orange hover:text-dark-grayish-blue", undefined: "border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900"'
+                          >
+                            <ion-icon
+                              class="p-2 text-base"
+                              name="notifications"
+                            ></ion-icon>
+                            <span className="truncate">Notifications</span>
                           </Link>
 
                           <Link
@@ -90,39 +114,15 @@ const UserProfile = () => {
                           </Link>
 
                           <Link
-                            to="notifications"
+                            to="settings"
                             className=" text-dark-grayish-blue border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900 group border-l-4 px-3 py-2 flex items-center text-sm font-medium"
                             x-state-description='undefined: "bg-pale-orange border-orange text-dark-grayish-blue hover:bg-pale-orange hover:text-dark-grayish-blue", undefined: "border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900"'
                           >
                             <ion-icon
                               class="p-2 text-base"
-                              name="notifications"
+                              name="settings"
                             ></ion-icon>
-                            <span className="truncate">Notifications</span>
-                          </Link>
-
-                          <Link
-                            to="orders"
-                            className=" text-dark-grayish-blue border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900 group border-l-4 px-3 py-2 flex items-center text-sm font-medium"
-                            x-state-description='undefined: "bg-pale-orange border-orange text-dark-grayish-blue hover:bg-pale-orange hover:text-dark-grayish-blue", undefined: "border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900"'
-                          >
-                            <ion-icon
-                              class="p-2 text-base"
-                              name="basket"
-                            ></ion-icon>
-                            <span className="truncate">My orders</span>
-                          </Link>
-
-                          <Link
-                            to="addresses"
-                            className=" text-dark-grayish-blue border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900 group border-l-4 px-3 py-2 flex items-center text-sm font-medium"
-                            x-state-description='undefined: "bg-pale-orange border-orange text-dark-grayish-blue hover:bg-pale-orange hover:text-dark-grayish-blue", undefined: "border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900"'
-                          >
-                            <ion-icon
-                              class="p-2 text-base"
-                              name="location"
-                            ></ion-icon>
-                            <span className="truncate">My Address</span>
+                            <span className="truncate">Account setting</span>
                           </Link>
                           <hr className="text-grayish-blue" />
                           <button className="text-orange flex items-center px-3 py-2">
@@ -135,112 +135,7 @@ const UserProfile = () => {
                         </nav>
                       </div>
                       <div className="bg-white flex-1 rounded-lg shadow-md p-8">
-                        <h3 className="text-xl leading-6 font-bold text-gray-900">
-                          My Account
-                        </h3>
-                        <p className="mt-1 max-w-2xl text-sm text-gray-500">
-                          Personal details and application.
-                        </p>
-                        <hr className="border-b border-grayish-blue mt-3 mb-8" />
-                        <dl className="grid grid-cols-1 gap-x-4 sm:grid-cols-1 divide-y divide-gray-200">
-                          {/* <div className="mt-5 sm:col-span-1"> */}
-                          <div className="sm:grid sm:grid-cols-3 sm:gap-4 py-4">
-                            <dt className="text-sm font-medium text-dark-grayish-blue px-2">
-                              Full Name
-                            </dt>
-                            <dd className="mt-1 flex text-sm text-very-dark-blue sm:mt-0 sm:col-span-2">
-                              <span className="sm:flex flex-grow">
-                                <input
-                                  className="focus:outline-pale-orange px-2 md:p-2 bg-white w-full sm:text-right pr-0"
-                                  type="text"
-                                  value={userInfo.firstname}
-                                  disabled
-                                />
-                                <input
-                                  className="focus:outline-pale-orange px-2 md:p-2 bg-white w-full"
-                                  type="text"
-                                  value={userInfo.lastname}
-                                  disabled
-                                />
-                              </span>
-                              <span className="ml-4 flex-shrink-0">
-                                <button
-                                  type="button"
-                                  className="bg-pale-orange rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none text-orange px-3 py-1"
-                                >
-                                  Edit
-                                </button>
-                              </span>
-                            </dd>
-                          </div>
-                          <div className="sm:grid sm:grid-cols-3 sm:gap-4 py-4">
-                            <dt className="text-sm font-medium text-dark-grayish-blue px-2">
-                              Email
-                            </dt>
-                            <dd className="mt-1 flex text-sm text-very-dark-blue sm:mt-0 sm:col-span-2">
-                              <input
-                                className="focus:outline-pale-orange p-2 bg-white w-full sm:text-center"
-                                type="text"
-                                value={userInfo.email}
-                                disabled
-                              />
-
-                              <span className="ml-4 flex-shrink-0">
-                                <button
-                                  type="button"
-                                  className="bg-pale-orange rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none text-orange px-3 py-1"
-                                >
-                                  Edit
-                                </button>
-                              </span>
-                            </dd>
-                          </div>
-                          <div className="sm:grid sm:grid-cols-3 sm:gap-4 py-4">
-                            <dt className="text-sm font-medium text-dark-grayish-blue px-2">
-                              Phone Number
-                            </dt>
-                            <dd className="mt-1 flex text-sm text-very-dark-blue sm:mt-0 sm:col-span-2">
-                              <input
-                                className="focus:outline-pale-orange p-2 bg-white w-full sm:text-center"
-                                type="text"
-                                value="08066634234"
-                                disabled
-                              />
-
-                              <span className="ml-4 flex-shrink-0">
-                                <button
-                                  type="button"
-                                  className="bg-pale-orange rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none text-orange px-3 py-1"
-                                >
-                                  Edit
-                                </button>
-                              </span>
-                            </dd>
-                          </div>
-                          <div className="sm:grid sm:grid-cols-3 sm:gap-4 py-4">
-                            <dt className="text-sm font-medium text-dark-grayish-blue px-2">
-                              Gender
-                            </dt>
-                            <dd className="mt-1 flex text-sm text-very-dark-blue sm:mt-0 sm:col-span-2">
-                              <input
-                                className="focus:outline-pale-orange p-2 bg-white w-full sm:text-center"
-                                type="text"
-                                value="Female"
-                                disabled
-                              />
-
-                              <span className="ml-4 flex-shrink-0">
-                                <button
-                                  type="button"
-                                  className="bg-pale-orange rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none text-orange px-3 py-1"
-                                >
-                                  Edit
-                                </button>
-                              </span>
-                            </dd>
-                          </div>
-                          {/* </div> */}
-                        </dl>
+                        <Outlet />
                       </div>
                     </div>
                   </>
