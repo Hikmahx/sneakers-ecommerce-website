73e50f3ea51ab5e9d6d1773926d91fedc3012faa
Hikmahx <hikmayousuph@gmail.com>
2022-07-22 22:53:25 +0100

    user profile routes added

--- client/src/routes/MyRoutes.jsx
+++ client/src/routes/MyRoutes.jsx
@@ -15,6 +15,12 @@ import NotFound from "../pages/NotFound";
 import Checkout from "../pages/Checkout";
 import { useSelector } from "react-redux";
 import UserProfile from "../pages/User/UserProfile";
+import Settings from "../pages/User/Profile/Settings";
+import MyAccount from "../pages/User/Profile/MyAccount";
+import MyOrders from "../pages/User/Profile/MyOrders";
+import MyAddress from "../pages/User/Profile/MyAddress";
+import Notifications from "../pages/User/Profile/Notifications";
+import Password from "../pages/User/Profile/Password";
 
 const MyRoutes = () => {
   const user = false;
@@ -36,7 +42,14 @@ const MyRoutes = () => {
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={user ? <Navigate to="/" replace={true}  /> : <Login />} />
         <Route path="/register" element={user ? <Navigate to="/" replace={true}  /> : <Register />} />
-        <Route path="/user-profile" element={ <UserProfile/>}/>
+        <Route path="/user-profile" element={ <UserProfile/>}>
+          <Route path="" element={<MyAccount />} />
+          <Route path="orders" element={<MyOrders />} />
+          <Route path="addresses" element={<MyAddress />} />
+          <Route path="notifications" element={<Notifications />} />
+          <Route path="password" element={<Password />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
         <Route path="/checkout" element={ cartItems.length <1 ? <Navigate to="/products" replace={true}  /> : <Checkout />} />
       </Routes>
     </Layout>
