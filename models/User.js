f7ad433708462198554d9f3b99556d0e11122ffc
Hikmahx <hikmayousuph@gmail.com>
2022-07-25 20:17:06 +0100

    User model updated

--- models/User.js
+++ models/User.js
@@ -2,12 +2,14 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
   {
-    firstname: { type: String, required: true, unique: true },
-    lastname: { type: String, required: true, unique: true },
+    firstname: { type: String, required: true},
+    lastname: { type: String, required: true},
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    isAdmin : {type: Boolean, default: false}
+    isAdmin : {type: Boolean, default: false},
+    phone: String,
+    gender: String
   },
   { timestamps: true }
 );
