3943165da4380f8c07c1a251b0e98cf3315bcc72
Hikmahx <hikmayousuph@gmail.com>
2022-07-26 01:11:04 +0100

    component updated

--- client/src/pages/User/UserProfile.jsx
+++ client/src/pages/User/UserProfile.jsx
@@ -20,166 +20,157 @@ const UserProfile = () => {
   return (
     <section className="h-auto pt-2 min-h-[80vh] bg-[#f9f9f9]">
       <div className=" max-w-xl lg:max-w-7xl relative px-5 py-20 items-center mx-auto lg:mx-20 xl:mx-28 2xl:mx-40 3xl:mx-auto lg:px-1 xl:px-3 2xl:px-1">
-        {!error ? (
-          <>
-            {loading ? (
-              <Loading />
-            ) : (
-              <>
-                {userInfo ? (
+        <div className="flex gap-x-4 flex-col lg:flex-row">
+          <div className="lg:bg-white lg:w-1/4 rounded-lg lg:shadow-md py-4">
+            <div className="profile-img-wrapper w-32 h-32 bg-grayish-blue rounded-full mx-auto relative">
+              <button className="w-5 h-5 absolute right-3">
+                <ion-icon
+                  class="text-very-dark-blue text-xl"
+                  name="create"
+                ></ion-icon>
+              </button>
+            </div>
+            <h3 className="capitalize text-lg text-center my-6">
+              {/* Welcome back{" "} */}
+              <div className="font-bold ">
+                {userInfo && (
                   <>
-                    {" "}
-                    <div className="flex gap-x-4 flex-col lg:flex-row">
-                      <div className="lg:bg-white lg:w-1/4 rounded-lg lg:shadow-md py-4">
-                        <div className="profile-img-wrapper w-32 h-32 bg-grayish-blue rounded-full mx-auto relative">
-                          <button className="w-5 h-5 absolute right-3">
-                            <ion-icon
-                              class="text-very-dark-blue text-xl"
-                              name="create"
-                            ></ion-icon>
-                          </button>
-                        </div>
-                        <h3 className="capitalize text-lg text-center my-6">
-                          {/* Welcome back{" "} */}
-                          <div className="font-bold ">
-                            {" "}
-                            {userInfo.firstname} {userInfo.lastname}
-                          </div>
-                        </h3>
-
-                        <nav className="space-y-1 bg-white">
-                          <NavLink
-                            to=""
-                            className={({ isActive }) =>
-                            "text-dark-grayish-blue group  px-3 py-2 flex items-center text-sm font-medium" +
-                            (!isActive ? " hover:bg-light-grayish-blue" : " border-l-4 bg-pale-orange border-orange hover:bg-pale-orange" )
-                          }
-                          end
-                            aria-current="page"
-                            x-state-description='Current: "bg-pale-orange border-orange text-dark-grayish-blue", Default: "border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900"'
-                          >
-                            {/* <ion-icon class="p-2 text-base" name="contact"></ion-icon> */}
-                            <ion-icon
-                              class="p-2 text-base"
-                              name="person"
-                            ></ion-icon>
-                            {/* <i clasName="fa-light fa-circle-user text-lg"></i> */}
-
-                            <span className="truncate">My Account</span>
-                          </NavLink>
-
-                          <NavLink
-                            to="orders"
-                            className={({ isActive }) =>
-                            "text-dark-grayish-blue group  px-3 py-2 flex items-center text-sm font-medium" +
-                            (!isActive ? " hover:bg-light-grayish-blue" : " border-l-4 bg-pale-orange border-orange hover:bg-pale-orange" )
-                            }
-                            x-state-description='undefined: "bg-pale-orange border-orange text-dark-grayish-blue", undefined: "border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900"'
-                          >
-                            <ion-icon
-                              class="p-2 text-base"
-                              name="basket"
-                            ></ion-icon>
-                            <span className="truncate">My Orders</span>
-                          </NavLink>
-
-                          <NavLink
-                            to="addresses"
-                            className={({ isActive }) =>
-                            "text-dark-grayish-blue group  px-3 py-2 flex items-center text-sm font-medium" +
-                            (!isActive ? " hover:bg-light-grayish-blue" : " border-l-4 bg-pale-orange border-orange hover:bg-pale-orange" )
-                          }
-                          x-state-description='undefined: "bg-pale-orange border-orange text-dark-grayish-blue", undefined: "border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900"'
-                          >
-                            <ion-icon
-                              class="p-2 text-base"
-                              name="location"
-                            ></ion-icon>
-                            <span className="truncate">My Address</span>
-                          </NavLink>
-
-                          <NavLink
-                            to="notifications"
-                            className={({ isActive }) =>
-                            "text-dark-grayish-blue group  px-3 py-2 flex items-center text-sm font-medium" +
-                            (!isActive ? " hover:bg-light-grayish-blue" : " border-l-4 bg-pale-orange border-orange hover:bg-pale-orange" )
-                          }
-                            x-state-description='undefined: "bg-pale-orange border-orange text-dark-grayish-blue", undefined: "border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900"'
-                          >
-                            <ion-icon
-                              class="p-2 text-base"
-                              name="notifications"
-                            ></ion-icon>
-                            <span className="truncate">Notifications</span>
-                          </NavLink>
-
-                          <NavLink
-                            to="password"
-                            className={({ isActive }) =>
-                            "text-dark-grayish-blue group  px-3 py-2 flex items-center text-sm font-medium" +
-                            (!isActive ? " hover:bg-light-grayish-blue" : " border-l-4 bg-pale-orange border-orange hover:bg-pale-orange" )
-                          }
-                            x-state-description='undefined: "bg-pale-orange border-orange text-dark-grayish-blue", undefined: "border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900"'
-                          >
-                            {/* <ion-icon class="p-2 text-base" name="unlock"></ion-icon> */}
-                            <ion-icon
-                              class="p-2 text-base"
-                              name="key"
-                            ></ion-icon>
-                            <span className="truncate">Password</span>
-                          </NavLink>
-
-                          <NavLink
-                            to="settings"
-                            className={({ isActive }) =>
-                            "text-dark-grayish-blue group  px-3 py-2 flex items-center text-sm font-medium" +
-                            (!isActive ? " hover:bg-light-grayish-blue" : " border-l-4 bg-pale-orange border-orange hover:bg-pale-orange" )
-                          }
-                            x-state-description='undefined: "bg-pale-orange border-orange text-dark-grayish-blue", undefined: "border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900"'
-                          >
-                            <ion-icon
-                              class="p-2 text-base"
-                              name="settings"
-                            ></ion-icon>
-                            <span className="truncate">Account setting</span>
-                          </NavLink>
-                          <hr className="text-grayish-blue" />
-                          <button className="text-orange flex items-center px-3 py-2">
-                            <ion-icon
-                              class="p-2 text-base"
-                              name="log-out"
-                            ></ion-icon>
-                            <span className="truncate">Log out</span>
-                          </button>
-                        </nav>
-                      </div>
-                      <div className="bg-white flex-1 rounded-lg shadow-md p-8 h-fit">
-                        <Outlet />
-                      </div>
-                    </div>
+                    {userInfo.firstname} {userInfo.lastname}
                   </>
+                )}
+              </div>
+            </h3>
+
+            <nav className="space-y-1 bg-white">
+              <NavLink
+                to=""
+                className={({ isActive }) =>
+                  "text-dark-grayish-blue group  px-3 py-2 flex items-center text-sm font-medium" +
+                  (!isActive
+                    ? " hover:bg-light-grayish-blue"
+                    : " border-l-4 bg-pale-orange border-orange hover:bg-pale-orange")
+                }
+                end
+                aria-current="page"
+                x-state-description='Current: "bg-pale-orange border-orange text-dark-grayish-blue", Default: "border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900"'
+              >
+                {/* <ion-icon class="p-2 text-base" name="contact"></ion-icon> */}
+                <ion-icon class="p-2 text-base" name="person"></ion-icon>
+                {/* <i clasName="fa-light fa-circle-user text-lg"></i> */}
+
+                <span className="truncate">My Account</span>
+              </NavLink>
+
+              <NavLink
+                to="orders"
+                className={({ isActive }) =>
+                  "text-dark-grayish-blue group  px-3 py-2 flex items-center text-sm font-medium" +
+                  (!isActive
+                    ? " hover:bg-light-grayish-blue"
+                    : " border-l-4 bg-pale-orange border-orange hover:bg-pale-orange")
+                }
+                x-state-description='undefined: "bg-pale-orange border-orange text-dark-grayish-blue", undefined: "border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900"'
+              >
+                <ion-icon class="p-2 text-base" name="basket"></ion-icon>
+                <span className="truncate">My Orders</span>
+              </NavLink>
+
+              <NavLink
+                to="addresses"
+                className={({ isActive }) =>
+                  "text-dark-grayish-blue group  px-3 py-2 flex items-center text-sm font-medium" +
+                  (!isActive
+                    ? " hover:bg-light-grayish-blue"
+                    : " border-l-4 bg-pale-orange border-orange hover:bg-pale-orange")
+                }
+                x-state-description='undefined: "bg-pale-orange border-orange text-dark-grayish-blue", undefined: "border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900"'
+              >
+                <ion-icon class="p-2 text-base" name="location"></ion-icon>
+                <span className="truncate">My Address</span>
+              </NavLink>
+
+              <NavLink
+                to="notifications"
+                className={({ isActive }) =>
+                  "text-dark-grayish-blue group  px-3 py-2 flex items-center text-sm font-medium" +
+                  (!isActive
+                    ? " hover:bg-light-grayish-blue"
+                    : " border-l-4 bg-pale-orange border-orange hover:bg-pale-orange")
+                }
+                x-state-description='undefined: "bg-pale-orange border-orange text-dark-grayish-blue", undefined: "border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900"'
+              >
+                <ion-icon class="p-2 text-base" name="notifications"></ion-icon>
+                <span className="truncate">Notifications</span>
+              </NavLink>
+
+              <NavLink
+                to="password"
+                className={({ isActive }) =>
+                  "text-dark-grayish-blue group  px-3 py-2 flex items-center text-sm font-medium" +
+                  (!isActive
+                    ? " hover:bg-light-grayish-blue"
+                    : " border-l-4 bg-pale-orange border-orange hover:bg-pale-orange")
+                }
+                x-state-description='undefined: "bg-pale-orange border-orange text-dark-grayish-blue", undefined: "border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900"'
+              >
+                {/* <ion-icon class="p-2 text-base" name="unlock"></ion-icon> */}
+                <ion-icon class="p-2 text-base" name="key"></ion-icon>
+                <span className="truncate">Password</span>
+              </NavLink>
+
+              <NavLink
+                to="settings"
+                className={({ isActive }) =>
+                  "text-dark-grayish-blue group  px-3 py-2 flex items-center text-sm font-medium" +
+                  (!isActive
+                    ? " hover:bg-light-grayish-blue"
+                    : " border-l-4 bg-pale-orange border-orange hover:bg-pale-orange")
+                }
+                x-state-description='undefined: "bg-pale-orange border-orange text-dark-grayish-blue", undefined: "border-transparent text-gray-900 hover:bg-gray-50 hover:text-gray-900"'
+              >
+                <ion-icon class="p-2 text-base" name="settings"></ion-icon>
+                <span className="truncate">Account setting</span>
+              </NavLink>
+              <hr className="text-grayish-blue" />
+              <button className="text-orange flex items-center px-3 py-2">
+                <ion-icon class="p-2 text-base" name="log-out"></ion-icon>
+                <span className="truncate">Log out</span>
+              </button>
+            </nav>
+          </div>
+          <div className="bg-white flex-1 rounded-lg shadow-md p-8">
+            {!error ? (
+              <>
+                {loading ? (
+                  <div className=" w-full h-full flex items-center justify-center">
+                    <Loading />
+                  </div>
                 ) : (
                   <>
-                    Please{" "}
-                    <NavLink
-                      to="/login"
-                      className="text-sm border-b-2 border-b-orange font-bold"
-                    >
-                      Login
-                    </NavLink>{" "}
-                    to view this page
+                    {userInfo ? (
+                      <Outlet />
+                    ) : (
+                      <>
+                        Please{" "}
+                        <NavLink
+                          to="/login"
+                          className="text-sm border-b-2 border-b-orange font-bold"
+                        >
+                          Login
+                        </NavLink>{" "}
+                        to view this page
+                      </>
+                    )}
                   </>
                 )}
               </>
-            )}
-          </>
-        ) : (
-          <>
-            <p className=" mt-20 text-center text-very-dark-blue">
-              {userErrorMsg}
-            </p>
-          </>
-        )}{" "}
+            ) : (
+              <p className=" mt-20 text-center text-very-dark-blue">
+                {userErrorMsg}
+              </p>
+            )}{" "}
+          </div>
+        </div>
       </div>
     </section>
   );
