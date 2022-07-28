8cb4d626990da5d280409e9641555a734395a875
Hikmahx <hikmayousuph@gmail.com>
2022-07-28 23:12:58 +0100

    cart model updated

--- models/Cart.js
+++ models/Cart.js
@@ -2,13 +2,23 @@ const mongoose = require("mongoose");
 
 const CartSchema = new mongoose.Schema(
   {
-    userId: { type: String, required: true },
-    products: [
-      {
-        productId: { type: String },
-        quantity: { type: Number, default: 1 },
-      },
-    ],
+    // userId: { type: String, required: true },
+    _id: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    products: {
+      type: Array,
+      unique: true
+    }
+    // products:
+    // [
+    //   {
+    //     productId: { type: String },
+    //     quantity: { type: Number, default: 1 },
+    //   },
+    // ],
   },
   { timestamps: true }
 );
