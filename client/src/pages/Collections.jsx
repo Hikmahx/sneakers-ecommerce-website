e827591519f495a1f98aa948bb10ef24380b6820
Hikmahx <hikmayousuph@gmail.com>
2022-08-24 23:34:33 -1100

    component updated

--- client/src/pages/Collections.jsx
+++ client/src/pages/Collections.jsx
@@ -8,7 +8,7 @@ const Collections = () => {
   return (
     <section className='h-auto lg:pt-2 min-h-[80vh]'>
       <div className='max-w-xl sm:max-w-4xl lg:max-w-7xl relative px-5 pt-20 pb-12 items-center mx-auto lg:mx-20 xl:mx-28 2xl:mx-40 3xl:mx-auto lg:pb-2 lg:px-1 xl:px-3 2xl:px-1'>
-        <h2 className='product capitalize text-white font-bold text-center relative z-10 lg:text-left text-3xl sm:text-4xl sm:leading-none pb-16 px-8'>
+        <h2 className='product capitalize text-white font-bold text-center relative z-[1] lg:text-left text-3xl sm:text-4xl sm:leading-none pb-16 px-8'>
           Collections
         </h2>
         <div className='absolute top-0 left-0 -z-0 bg-dark-grayish-blue w-full h-48 lg:rounded-md overflow-hidden'>
--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
   return (
     <section className="bg-light-grayish-blue h-auto pt-2 min-h-[80vh]">
       <div className="max-w-xl sm:max-w-4xl lg:max-w-7xl relative px-5 pt-20 pb-12 items-center mx-auto lg:mx-20 xl:mx-28 2xl:mx-40 3xl:mx-auto lg:pb-2 lg:px-1 xl:px-3 2xl:px-1">
-        <h2 className="product capitalize text-white font-bold text-center relative z-10 lg:text-left text-3xl sm:text-4xl sm:leading-none pb-3 px-8">
+        <h2 className="product capitalize text-white font-bold text-center relative z-[1] lg:text-left text-3xl sm:text-4xl sm:leading-none pb-3 px-8">
           Contact
         </h2>
         <div className="absolute top-0 left-0 bg-dark-grayish-blue w-full h-48 rounded-md overflow-hidden">
