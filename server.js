b53d1755b24a5c94b82bc7d158d0e77f89a50476
Hikmahx <hikmayousuph@gmail.com>
2023-06-06 17:58:37 +0100

    server.js

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const connectDB = require("./config/db");
-const path = require("path")
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const path = require('path');
 
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path: './config/config.env' });
 connectDB();
 
-const app = express()
+const app = express();
 
-app.use(express.json({ extended: false })); 
+app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
-
 // ROUTES
-app.use('/api/auth', require('./routes/auth'))
+app.use('/api/auth', require("./routes/auth"));
 app.use("/api/users", require("./routes/user"));
 app.use("/api/products", require("./routes/product"));
 app.use("/api/cart", require("./routes/cart"));
@@ -22,20 +21,14 @@ app.use("/api/orders", require("./routes/order"));
 app.use("/api/address", require("./routes/address"));
 app.use("", require("./routes/stripe"));
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../frontend', 'build')));
-//   app.get('/*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
-//   })
-// }
-
-
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  // Serve static files from the React app
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
+  // Route all other requests to the React app
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+  });
 }
 
-app.listen(PORT, ()=> console.log('This is listening on PORT: ' + PORT))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
